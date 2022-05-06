009d4555d39e1eee11ebff00e6ead4eff597ad06
Chef4ku <chenafcnd@gmail.com>
2022-05-06 09:16:56 -0300

    state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import React from 'react';
 import './App.css';
 
 
-const HelloWorld = (p) => <div id="hello">Hello World {p.text}</div>;
+// const HelloWorld = (props) => <div id="hello">Hello World {props.text}</div>;
 
+class HelloWorld extends React.Component {
+  
+  state = {
+    show: false,
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <p>Hello World {this.props.text}</p>
+
+          <button onClick={this.toggleShow}>Hide</button>
+        </div>
+      );
+    } else {
+      return (
+        <div id="hello">
+          <button onClick={this.toggleShow}>Show</button>
+        </div>
+      );  
+    }
+  }
+}
 
 function App() {
   return (
